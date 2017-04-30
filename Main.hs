943ef63d84825b11e40cf9998c2ed19459aa87ed
Steven Shaw <steven@steshaw.org>
2017-04-30 15:19:07 +1000

    Update to CircleCI 2.0

--- Main.hs
+++ Main.hs
@@ -5,7 +5,7 @@ import Web.Scotty
 main :: IO ()
 main = scotty 3000 $ do
   get "/" $ do
-    html $ "<h1>Hello world!</h1>"
+    html $ "<h1>hello-world!</h1>"
   get "/:word" $ do
     beam <- param "word"
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
