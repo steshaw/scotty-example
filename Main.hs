afc4f0a02024f7a691dace1b848ae1737d6a31de
Steven Shaw <steven@steshaw.org>
2017-04-30 14:10:07 +1000

    / is "hello world"

--- Main.hs
+++ Main.hs
@@ -4,6 +4,8 @@ import Web.Scotty
 
 main :: IO ()
 main = scotty 3000 $ do
+  get "/" $ do
+    html $ "<h1>Hello world!</h1>"
   get "/:word" $ do
     beam <- param "word"
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
